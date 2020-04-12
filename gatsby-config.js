f3ac9f46bb93d34aef1459b14d5e6c46065abfec
Morgan Peters <morleegan@yahoo.com>
2020-04-12 18:32:59 -0500

    init

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: `Morleegan`,
+    description: `A portfolio website for Morgan Peters.`,
+    author: `@morleegan`,
+  },
+  plugins: ['gatsby-plugin-typescript']
+};
